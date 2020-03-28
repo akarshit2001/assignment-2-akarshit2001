7840f23c0ac0a7e2cf3ff6571f4105f7a07188f4
akarshit <akarshitsrivastava33@gmail.com>
2020-03-28 12:00:31 +0530

    Nodeclass is done

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,41 @@
 package problem1.node;
 
 public class TreeNode {
+    private TreeNode left;
+    private TreeNode right;
+    private int data;
+
+    public TreeNode(TreeNode left, TreeNode right, int data) {
+        this.left = left;
+        this.right = right;
+        this.data = data;
+    }
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public int getData() {
+        return data;
+    }
 }
