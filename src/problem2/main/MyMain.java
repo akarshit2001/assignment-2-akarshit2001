c0976ec3630fe09747bdd3bc05235c3bd16f3123
akarshit <akarshitsrivastava33@gmail.com>
2020-03-29 13:22:56 +0530

    problem2 is done

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -6,6 +6,23 @@
  */
 package problem2.main;
 // executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree bin = new MyBinarySearchTree();
+        bin.insert(10);
+        bin.insert(20);
+        bin.insert(3);
+        bin.insert(4);
+        bin.insert(50);
+        System.out.println("Traverse preorder");
+        bin.traversePreorder(bin.getRoot());
+        System.out.println("\n");
+        System.out.println("Traverse postorder");
+        bin.traversePostorder(bin.getRoot());
+
+    }
+
 }
