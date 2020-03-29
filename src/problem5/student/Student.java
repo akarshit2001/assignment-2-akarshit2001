19eff66aa51fb9032124d4f95cf58c0c401f780d
akarshit <akarshitsrivastava33@gmail.com>
2020-03-29 19:38:59 +0530

    Student.java is done

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,37 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    private String Name;
+    private int backlog;
+    private int apperingCount;
+
+    public Student(String name, int backlog, int apperingCount) {
+        Name = name;
+        this.backlog = backlog;
+        this.apperingCount = apperingCount;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(int backlog) {
+        this.backlog = backlog;
+    }
+
+    public int getApperingCount() {
+        return apperingCount;
+    }
+
+    public void setApperingCount(int apperingCount) {
+        this.apperingCount = apperingCount;
+    }
 }
