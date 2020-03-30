6de52b5395cad2d362c213f41219de85d65b9f4b
akarshit <akarshitsrivastava33@gmail.com>
2020-03-30 23:47:13 +0530

    finally done

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -9,17 +9,16 @@ package problem3.main;
 // use problem5.student.Student class to create object of student
 
 import problem3.myqueue.MyPriorityQueue;
-import problem3.node.Node;
 
 public class MyMain {
     public static void main(String[] args) {
-        System.out.println("df");
         MyPriorityQueue myPriorityQueue= new MyPriorityQueue();
         myPriorityQueue.push("Akarshit",1);
         myPriorityQueue.push("Alankrit",2);
         myPriorityQueue.push("Alankrit",0);
         myPriorityQueue.push("Abhilasha",3);
         String pee= myPriorityQueue.peek();
+        System.out.println("Display data");
         myPriorityQueue.print();
     }
 }
